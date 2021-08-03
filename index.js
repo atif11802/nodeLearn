bbce9c26fbd34530097d251f95f1e5fe0f2831c1
ratul <ratul0947@gmail.com>
2021-08-03 23:07:45 +0600

    7th video of playlist

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
+//async file system operations
+
 const fs = require("fs");
 
-//make folder
-// const folderName = "Thapa";
+//add data
+// fs.writeFile("read.txt", "7th video of playlist", (err) => {
+// 	console.log("file is created");
+// 	console.log(err);
+// });
 
-// try {
-// 	if (!fs.existsSync(folderName)) {
-// 		fs.mkdirSync(folderName);
-// 	}
-// } catch (err) {
-// 	console.error("file already exist");
-// }
+//append file
 
-// making file and data
-// fs.writeFileSync(
-// 	"./Thapa/bio.txt",
-// 	"hello this is ratul,watching thapa technical"
-// );
+// fs.appendFile("read.txt", " appending here", (err) => {
+// 	console.log(err);
+// });
 
-//append data to the file
-// fs.appendFileSync("./Thapa/bio.txt", " so this is 6th video of the playlist");
-
-//read data without buffer data
-// const data = fs.readFileSync("./Thapa/bio.txt", "utf8");
-// console.log(data);
-
-//rename fila name
-// fs.renameSync("Thapa/bio.txt", "Thapa/myBio.txt");
-
-//delete a file
-// fs.unlinkSync("Thapa/myBio.txt");
-
-//delete a folder
-fs.rmdirSync("Thapa");
+//read file in nodejs
+fs.readFile("read.txt", "utf-8", (err, data) => {
+	if (err) throw err;
+	console.log(data);
+});
