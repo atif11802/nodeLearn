b83694b5073e0fb5046d19a532f810ea66449f12
ratul <ratul0947@gmail.com>
2021-08-04 20:39:42 +0600

    own module create

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-const os = require("os");
+// const add = require("./operator");
+const { add, sub, mult, name } = require("./operator");
 
-//to check bit(32 0r 64 bit)
-// console.log(os.arch());
-
-//memory available in ram
-// const freeMemory = os.freemem();
-
-// console.log(freeMemory);
-
-// console.log(`${freeMemory / 1024 / 1024 / 1024} is my memory`);
-
-// total memory  in ram
-// const totalMemory = os.totalmem();
-// console.log(`${totalMemory / 1024 / 1024 / 1024} is my memory`);
-
-//hotname
-// console.log(os.hostname());
-
-//platform
-
-// console.log(os.platform());
-
-//dir
-// console.log(os.tmpdir());
-
-//od
-console.log(os.type());
+console.log(add(5, 15));
+console.log(sub(50, 15));
+console.log(mult(5, 15));
+console.log(name);
