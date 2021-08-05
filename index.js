d73950f3e4716eec881b15595ab949b570970862
ratul <ratul0947@gmail.com>
2021-08-05 22:42:36 +0600

    routing with node js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-	res.end("hello from the other sides");
+	// console.log(req.url);
+	if (req.url === "/") {
+		res.end("hello from the home");
+	} else if (req.url === "/about") {
+		res.end("hello from the about");
+	} else if (req.url === "/contact") {
+		res.writeHead(200, { "Content-type": "text/html" });
+		res.end("hello from the contaxt 122");
+	} else {
+		res.writeHead(404, { "Content-type": "text/html" });
+		res.end("<h1>404 error </h1>");
+	}
 });
 
 server.listen(8000, "127.0.0.1", () => {
-	console.log("listen on 9000");
+	console.log("listen on 8000");
 });
