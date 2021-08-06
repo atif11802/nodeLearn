5ee9a11d2e15c65bc335ffe05f5245e7a65941b7
ratul <ratul0947@gmail.com>
2021-08-06 18:58:33 +0600

    api making with node

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
+const http = require("http");
+const url = require("url");
+
 const fs = require("fs");
 
-const bioData = {
-	name: "atif",
-	age: 22,
-	nationality: "bangladeshi",
-};
+const server = http.createServer((req, res) => {
+	const data = fs.readFileSync("userApi.json", "utf-8");
 
-// // console.log(bioData.nationality);
-// const jsonData = JSON.stringify(bioData);
-
-// console.log(jsonData);
-
-// console.log(JSON.parse(jsonData));
-
-// 1 convert to jsonData ...
-//2 how to add this another file
-//3 read file
-//4 conver json to obj
-//consolelog
-
-const jsonData = JSON.stringify(bioData);
-
-// fs.writeFile("json1.json", jsonData, (err) => {
-// 	console.log(err);
-// });
-
-fs.readFile("json1.json", "utf-8", (err, data) => {
 	const objData = JSON.parse(data);
-	console.log(data, objData);
+	// console.log(req.url);
+	if (req.url === "/") {
+		res.end("hello from the home");
+	} else if (req.url === "/about") {
+		res.end("hello from the about");
+	} else if (req.url === "/contact") {
+		res.writeHead(200, { "Content-type": "text/html" });
+		res.end("hello from the contaxt 122");
+	} else if (req.url === "/userapi") {
+		res.writeHead(200, { "Content-type": "application/json" });
+		res.end(objData[2].name);
+	} else {
+		res.writeHead(404, { "Content-type": "text/html" });
+		res.end("<h1>404 error </h1>");
+	}
+});
+
+server.listen(8000, "127.0.0.1", () => {
+	console.log("listen on 8000");
 });
