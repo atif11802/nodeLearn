81199de98ec28290cedc55542ec4b49c5907ac47
ratul <ratul0947@gmail.com>
2021-08-10 20:20:43 +0600

    responsive website serving with nodejs

--- index.js
+++ index.js
@@ -1,56 +1,15 @@
 const express = require("express");
+
 const app = express();
 const port = 8000;
 const path = require("path");
-// console.log(__dirname);
 
-console.log(path.join(__dirname, "/public"));
-
-const staticPath = path.join(__dirname, "/public");
-//built in middleware
-app.use(express.static(staticPath));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	res.write(" <h1> hellooo atif</h1>");
-	res.write(" <h1> hellooo 1 atif</h1>");
-	res.write(" <h1> hellooo  2 atif</h1>");
-	res.send();
+	res.send("hello");
 });
 
-app.get("/about", (req, res) => {
-	res.send(" <h1> hellooo about</h1> ");
+app.listen(port, () => {
+	console.log(`listening to port ${port}`);
 });
-app.get("/contact", (req, res) => {
-	res.send(" <h1> hellooo contact</h1> ");
-});
-// app.get("/temp", (req, res) => {
-// 	res.send([
-// 		{
-// 			name: "atif",
-// 			id: 1,
-// 			nationality: "bangladeshi",
-// 		},
-// 		{
-// 			name: "aslam",
-// 			id: 1,
-// 			nationality: "bangladeshi",
-// 		},
-// 	]);
-// });
-
-// app.get("/temp", (req, res) => {
-// 	res.json([
-// 		{
-// 			name: "atif",
-// 			id: 1,
-// 			nationality: "bangladeshi",
-// 		},
-// 		{
-// 			name: "aslam",
-// 			id: 1,
-// 			nationality: "bangladeshi",
-// 		},
-// 	]);
-// });
-
-app.listen(port, () => console.log(`app listening on port ${port}!`));
