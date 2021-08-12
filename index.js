0f81a06baab4b14fc6275970404cdba8bc27bcef
ratul <ratul0947@gmail.com>
2021-08-12 23:05:26 +0600

    responsive or dynamic and partials using website

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
 const express = require("express");
-
 const app = express();
-const port = 8000;
 const path = require("path");
 const hbs = require("hbs");
 
-//to set the view engine
-app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
 
+app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "templates/views"));
 
 hbs.registerPartials(path.join(__dirname, "templates/partials"));
-const staticPath = path.join(__dirname, "public");
-
-app.use(express.static(staticPath));
-// app.use(express.static(path.join(__dirname, "/public")));
-
 app.get("/", (req, res) => {
-	res.render("index", { name: "atif aslam ratul" });
+	res.render("index", { name: "ratul" });
 });
-
-// app.get("/", (req, res) => {
-// 	res.send("hello");
-// });
 
 app.get("/about", (req, res) => {
-	res.render("about", { name: "atif  ratul" });
+	res.render("about");
 });
 
-///if you go about page then search more and that page doesnt exist than show something so /about/*
-
-app.get("/about/*", (req, res) => {
-	res.render("404", {
-		error: "oops!! this about page couldn't be found",
-	});
-});
-
-app.get("*", (req, res) => {
-	res.render("404", { error: "oops!! page didnt found" });
-});
-
-app.listen(port, () => {
-	console.log(`listening to port ${port}`);
+app.listen(8000, () => {
+	console.log("listening on");
 });
