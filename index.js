034f5c90f71864420adb82d59bac9122d3a6d2ea
ratul <ratul0947@gmail.com>
2021-08-12 18:48:59 +0600

    partialS

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const path = require("path");
+const hbs = require("hbs");
 
 //to set the view engine
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "templates"));
+
+app.set("views", path.join(__dirname, "templates/views"));
+
+hbs.registerPartials(path.join(__dirname, "templates/partials"));
 const staticPath = path.join(__dirname, "public");
 
 app.use(express.static(staticPath));
 // app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/about", (req, res) => {
-	res.render("index", { name: "atif aslam" });
-});
-app.get("/contact", (req, res) => {
-	res.render("about");
+app.get("/", (req, res) => {
+	res.render("index", { name: "atif aslam ratul" });
 });
 
 app.get("/", (req, res) => {
 	res.send("hello");
+});
+app.get("/about", (req, res) => {
+	res.render("about", { name: "atif  ratul" });
 });
 
 app.listen(port, () => {
